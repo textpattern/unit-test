e1eeb4e6fdacad88b8a9bb28cc199a280d1d4615
Stef Dawson <thatbloke@stefdawson.com>
2021-04-05 09:37:24 +0100

    Add version checking capability

---
+++ textpattern/vendors/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'da8f02cf043797d803409b22af74be9ba3cb4fce',
+    'name' => 'textpattern/unit-test',
+  ),
+  'versions' => 
+  array (
+    'composer/semver' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0ec124f57c7e23925c006cbad0de853e3aec3ba2',
+    ),
+    'textpattern/unit-test' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'da8f02cf043797d803409b22af74be9ba3cb4fce',
+    ),
+  ),
+);
